c7e2e1d822d368424ee9af14ec6f00da1a9f5b6a
Kenta_Kobyashi <k.kobayashi311@gmail.com>
2023-12-20 14:03:26 +0900

    20231220作り直し

---
+++ src/pages/components/CheckField.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "./styles/CheckField.module.css";
+
+type Props = {
+  prefectures:
+    | {
+        prefCode: number;
+        prefName: string;
+      }[];
+
+  onChange: (name: string, prefName: number, check: boolean) => void;
+};
+
+// 都道府県一覧のチェックボックスを表示するコンポーネント
+const CheckField: React.FC<Props> = ({ prefectures, onChange }) => {
+  return (
+    <>
+      <div>
+        {prefectures.map((prefecture) => (
+          <div key={prefecture.prefName}>
+            <input
+                type="checkbox"
+                name="Prefecture name"
+                onChange={(event) =>
+                onChange(
+                    prefecture.prefName,
+                    prefecture.prefCode,
+                    event.target.checked
+                )
+                }
+                id={"checkbox" + prefecture.prefCode}
+            />
+            <label htmlFor={"checkbox" + prefecture.prefCode}>
+                {prefecture.prefName.length === 3
+                ? "　" + prefecture.prefName
+                : prefecture.prefName}
+            </label>
+            </div>
+        ))}
+        </div>
+    </>
+    );
+};
+
+export default CheckField;
